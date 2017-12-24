ff9f9e544207f26d0683fc150569741e9df3b51b
emzarts <17zimmermane@gmail.com>
2017-12-24 13:36:55 -0500

    Started implementing PTUI_Client.java - main method presents user with game options

--- src/PTUI_Client.java
+++ src/PTUI_Client.java
@@ -1,5 +1,7 @@
+
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
 public class PTUI_Client implements Observer {
 
@@ -9,6 +11,29 @@ public class PTUI_Client implements Observer {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Tic Tac Toe!\nWould you like to:\n\ta) Start a new game\n\tb) Join a game\n\tc) Exit");
+        boolean begin = false;
+        while (!begin) {
+            begin = true;
+            switch (sc.nextLine().toLowerCase()) {
 
+                case "a":
+                    System.out.println("Choice a");
+                    // TODO asks for a port number and host
+                    // TODO starts a server on that port and waits for the other player
+                    break;
+                case "b":
+                    System.out.println("Choice b");
+                    // Todo asks for the port and host to connect to
+                    break;
+                case "c":
+                    System.exit(0);
+                    break;
+                default:
+                    begin = false;
+                    System.out.println("Please type a, b, or c");
+            }
+        }
     }
 }
