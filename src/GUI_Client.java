cb899d74ebaedebc455a46effc7df9187f4e3d80
emzarts <17zimmermane@gmail.com>
2017-12-24 13:24:21 -0500

    Added Server.java and made the Clients Observers.

--- src/GUI_Client.java
+++ src/GUI_Client.java
@@ -1,2 +1,14 @@
-public class GUI_Client {
+import java.util.Observable;
+import java.util.Observer;
+
+public class GUI_Client implements Observer {
+
+    @Override
+    public void update(Observable o, Object obj) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/PTUI_Client.java
+++ src/PTUI_Client.java
@@ -1,2 +1,14 @@
-public class PTUI_Client {
+import java.util.Observable;
+import java.util.Observer;
+
+public class PTUI_Client implements Observer {
+
+    @Override
+    public void update(Observable o, Object obj) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
